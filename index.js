659568481a53f055431c76607d716ecb24660869
Alex Chen <alexchen@dhcp-18-20-216-157.dyn.mit.edu>
2020-02-06 19:20:43 -0500

    hi

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-const resetAnimation = () => {
+document
+  .querySelector("#image")
+  .addEventListener("animationend", resetAnimation);
+
+function resetAnimation() {
   document.querySelector("#image").classList.remove("animated");
   document.querySelector("#image").classList.remove("fadeIn");
-  document
-    .querySelector("#image")
-    .removeEventListener("animationend", resetAnimation);
-};
+}
 
-const getImage = () => {
+function getImage() {
   const name = document.querySelector("#dogname").value;
   fetch(`https://dog.ceo/api/breed/${name}/images/random`)
-    .then(res => res.json())
-    .then(res => {
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(res) {
       if (res.code == 404) {
         document.querySelector("#error").style.visibility = "visible";
       } else {
         document.querySelector(
           "#image"
         ).style.backgroundImage = `url(${res.message})`;
-        document
-          .querySelector("#image")
-          .classList.add(["animated", "fadeIn", "delay-1s"]);
-        document
-          .querySelector("#image")
-          .addEventListener("animationend", resetAnimation);
+        document.querySelector("#image").classList.add("animated");
+        document.querySelector("#image").classList.add("fadeIn");
+        document.querySelector("#image").classList.add("delay-1s");
         document.querySelector("#error").style.visibility = "hidden";
       }
     });
-};
+}
 
-const getRandomImage = () => {
+function getRandomImage() {
   fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res => res.json())
-    .then(res => {
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(res) {
       document.querySelector(
         "#image"
       ).style.backgroundImage = `url(${res.message})`;
       document.querySelector("#image").classList.add("animated");
       document.querySelector("#image").classList.add("fadeIn");
       document.querySelector("#image").classList.add("delay-1s");
-      document
-        .querySelector("#image")
-        .addEventListener("animationend", resetAnimation);
       document.querySelector("#error").style.visibility = "hidden";
     });
-};
+}

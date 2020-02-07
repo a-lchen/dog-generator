de3045d31c4e6f6b0a9573b933dc19cbd82ae110
Cory Lynch <cjl2625@gmail.com>
2020-02-06 22:08:22 -0500

    Add arrow function for res.json()

--- index.js
+++ index.js
@@ -10,16 +10,12 @@ function resetAnimation() {
 function getImage() {
   const name = document.querySelector("#dogname").value;
   fetch(`https://dog.ceo/api/breed/${name}/images/random`)
-    .then(function(res) {
-      return res.json();
-    })
+    .then((res) => res.json())
     .then(function(res) {
       if (res.code == 404) {
         document.querySelector("#error").style.visibility = "visible";
       } else {
-        document.querySelector(
-          "#image"
-        ).style.backgroundImage = `url(${res.message})`;
+        document.querySelector("#image").style.backgroundImage = `url(${res.message})`;
         document.querySelector("#image").classList.add("animated");
         document.querySelector("#image").classList.add("fadeIn");
         document.querySelector("#image").classList.add("delay-1s");
@@ -30,13 +26,9 @@ function getImage() {
 
 function getRandomImage() {
   fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
     .then(function(res) {
-      return res.json();
-    })
-    .then(function(res) {
-      document.querySelector(
-        "#image"
-      ).style.backgroundImage = `url(${res.message})`;
+      document.querySelector("#image").style.backgroundImage = `url(${res.message})`;
       document.querySelector("#image").classList.add("animated");
       document.querySelector("#image").classList.add("fadeIn");
       document.querySelector("#image").classList.add("delay-1s");

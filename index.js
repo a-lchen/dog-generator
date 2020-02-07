7363b868a78998c64fe350a20c82961ef81927eb
Cory Lynch <cjl2625@gmail.com>
2020-02-06 22:05:17 -0500

    User arrow function for res.json

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ function onKeyPress(event) {
 function getImage() {
   const name = document.querySelector("#dogname").value;
   fetch(`https://dog.ceo/api/breed/${name}/images/random`)
-    .then(function(res) {
-      return res.json();
-    })
+    .then((res) => res.json())
     .then(function(res) {
       if (res.code == 404) {
         alert("Woof woof, we couldn't find that dog! Please try again.");
@@ -25,9 +23,7 @@ function getImage() {
 
 function getRandomImage() {
   fetch("https://dog.ceo/api/breeds/image/random")
-    .then(function(res) {
-      return res.json();
-    })
+    .then((res) => res.json())
     .then(function(res) {
       document.querySelector(
         "#image"

ff1f4cadd3863457cc0d1c7f916badda339b8d5b
Cory Lynch <cjl2625@gmail.com>
2020-02-07 22:12:15 -0500

    Tweak javascript workshop

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function getImage() {
-  const name = document.querySelector("#dogname").value;
+function getSpecificImage() {
+  const name = document.querySelector("#dog-name").value;
   fetch(`https://dog.ceo/api/breed/${name}/images/random`)
     .then((res) => res.json())
     .then(function(res) {
-      if (res.code == 404) {
+      if (res.status === "error") {
         alert("Woof woof, we couldn't find that dog! Please try again.");
       } else {
-        document.querySelector(
-          "#image"
-        ).style.backgroundImage = `url(${res.message})`;
+        document.querySelector("#dog-image").style.backgroundImage = `url(${res.message})`;
       }
     });
 }
@@ -17,8 +15,6 @@ function getRandomImage() {
   fetch("https://dog.ceo/api/breeds/image/random")
     .then((res) => res.json())
     .then(function(res) {
-      document.querySelector(
-        "#image"
-      ).style.backgroundImage = `url(${res.message})`;
+      document.querySelector("#dog-image").style.backgroundImage = `url(${res.message})`;
     });
 }
